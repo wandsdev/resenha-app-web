00fda7f36921e642cf9e540dac087827c01cea77
Wands <wandsdeveloper@gmail.com>
2022-02-05 21:22:15 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<v-app id="inspire">
+		<HeaderBar/>
+		<SideBar/>
+		<v-main>
+			<v-container class="py-4 px-4" fluid>
+				<router-view></router-view>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import HeaderBar from '@/components/layouts/HeaderBar';
+import SideBar from '@/components/layouts/SideBar';
+export default {
+	name: 'App',
+	components: { HeaderBar, SideBar },
+	data: () => ({
+		cards: ['Today', 'Yesterday'],
+	}),
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ src/components/layouts/HeaderBar.vue
@@ -0,0 +1,43 @@
+<template>
+	<v-app-bar
+		app
+		color="white"
+		elevation="2"
+	>
+		<v-container class="py-0 fill-height">
+			<v-btn v-for="link in links2" :key="link" text>
+				{{ link }}
+			</v-btn>
+
+			<v-spacer></v-spacer>
+
+			<v-responsive max-width="260">
+				<v-text-field
+					dense
+					flat
+					hide-details
+					rounded
+					solo-inverted
+				></v-text-field>
+			</v-responsive>
+		</v-container>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	name: "HeaderBar",
+	data: () => ({
+		links2: [
+			'Dashboard',
+			'Messages',
+			'Profile',
+			'Updates',
+		],
+	})
+}
+</script>
+
+<style scoped>
+
+</style>
